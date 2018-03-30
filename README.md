003c374086ffe44866bccca7043c5d96100f3d15
metin <metin@gulenayva.com>
2018-03-30 14:26:00 +0300

    -updated

--- README.md
+++ README.md
@@ -1,7 +1,49 @@
-keys:
+#JS DOM SCROLL POSITIONS
 
-display:grid with fixed example;
+*  How much have you scrolled?
 
-js edit dom height;
-getBoundingClientRect();
-addEventListener;
+```javascript
+window.pageYOffset || document.documentElement.scrollTop;
+```
+
+---
+
+*  How much your window scroll height?
+
+```javascript
+window.document.body.scrollHeight || window.document.body.clientHeight;
+```
+
+---
+
+*  How much your element height?
+
+```javascript
+element.scrollHeight;
+```
+
+---
+
+*  How much your window height ?
+
+```javascript
+window.innerHeight;
+```
+
+---
+
+*  How to end of the page bottom ?
+
+```javascript
+if (window.document.body.scrollHeight - window.innerHeight === window.pageYOffset) {
+   true;
+}
+```
+
+---
+
+*  How to get the distance from the top for an element?
+
+```javascript
+element.offsetTop;
+```
--- app.js
+++ app.js
@@ -3,24 +3,65 @@ window.addEventListener("scroll", () => {
    runHeight();
 });
 
+const scrollToTop = () => {
+   window.scroll({
+      top: 0, 
+      left: 0, 
+      behavior: 'smooth' 
+    });   
+}
+
 const runHeight = () => {
    const header = document.getElementsByClassName("header-root")[0];
    const menu = document.getElementsByClassName("menu")[0];
    const footer = document.getElementsByClassName("footer-root")[0];
+   const content = document.getElementsByClassName("content")[0];
+   console.log("---");
+   console.log("TOP-POSITION..." + window.pageYOffset);
+   console.log("WINDOW-SCROLL-POSITION..." + window.document.body.clientHeight);
+   console.log("WINDOW-POSITION..." + window.innerHeight);
+   console.log(
+      "END OF BOTTOM..." +
+         (window.document.body.scrollHeight - window.innerHeight ===
+            window.pageYOffset)
+   );
    currentPosition = window.pageYOffset || document.documentElement.scrollTop;
    let px = "px";
    let position = menu.getBoundingClientRect().top;
-   if (position >= 50) {
-      header.style.height = `${menu.getBoundingClientRect().top}${px}`;
-   } else if (position <= -400) {
-      if (previousPosition > currentPosition) {
-         setTimeout(() => {
+
+   if (window.pageYOffset === 0) {
+      header.style.height = "130px";
+      header.style.top = 0;
+      footer.style.height = "50px";
+      footer.style.bottom = 0;
+      console.log("top");
+   } else if (
+      window.document.body.scrollHeight - window.innerHeight ===
+      window.pageYOffset
+   ) {
+      header.style.height = "50px";
+      header.style.top = 0;
+      footer.style.height = "50px";
+      footer.style.bottom = 0;
+      console.log("bottom");
+   } else {
+      if (position >= 50) {
+         header.style.height = `${menu.getBoundingClientRect().top}${px}`;
+         console.log("50");
+      } else if (position <= -300) {
+         if (previousPosition > currentPosition) {
             header.style.height = "50px";
+            header.style.top = 0;
             footer.style.height = "50px";
-         }, 500);
-      } else {
-         header.style.height = 0;
-         footer.style.height = 0;
+            footer.style.bottom = 0;
+            console.log("up");
+         } else {
+            header.style.height = "50px";
+            header.style.top = "-50px";
+            footer.style.height = "50px";
+            footer.style.bottom = "-50px";
+            console.log("down");
+         }
       }
    }
    previousPosition = currentPosition;
